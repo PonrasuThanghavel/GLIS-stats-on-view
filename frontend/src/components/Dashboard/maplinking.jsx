d05bb4ac3aea9aecef2c554dc4e860803d0c310e
Ponrasu Thanghavel <145901854+PonrasuThanghavel@users.noreply.github.com>
2024-04-21 22:18:58 +0530

    updated maplinking component

--- frontend/src/components/Dashboard/maplinking.jsx
+++ frontend/src/components/Dashboard/maplinking.jsx
@@ -15,6 +15,7 @@ const Maplinking = () => {
       try {
         const response = await axios.get('https://glis-backend.onrender.com/api/geocode');
         setGeocode(response.data);
+        // Extracting unique zone types
         const types = response.data.map(geo => geo.Zone_type.toLowerCase());
         setZoneTypes([...new Set(types)]);
       } catch (error) {
@@ -34,50 +35,43 @@ const Maplinking = () => {
     setFilteredZone(event.target.value);
   };
 
-  const filteredGeocode = filteredZone
-    ? geocode.filter(geo => geo.Zone_type.toLowerCase() === filteredZone.toLowerCase())
-    : geocode;
+  const filteredGeocode = filteredZone ? geocode.filter(geo => geo.Zone_type.toLowerCase() === filteredZone.toLowerCase()) : geocode;
 
   return (
     <div>
       <div className="filter-container">
         <label className="filter-label" htmlFor="zoneFilter">Filter by Zone Type: </label>
-        
         <select className="filter-control" id="zoneFilter" onChange={handleFilterChange} value={filteredZone}>
           <option value="">All ({geocode.length})</option>
-
           {zoneTypes.map((type, index) => (
             <option key={index} value={type}>
               {type} ({geocode.filter(geo => geo.Zone_type.toLowerCase() === type).length})
             </option>
           ))}
-
         </select>
       </div>
-
       <MapContainer center={[13.0505, 80.2241]} zoom={12}>
         <TileLayer
-          attribution="GLIS"
+          attribution="mywork"
           url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
         />
 
         {filteredGeocode.map((geo, index) => (
           <Marker key={index} position={[geo.lat, geo.long]} icon={customIcon}>
             <Popup>
-              <div className="popup-container">
-                <h4>ID: {geo.ID}</h4>
-                <h4>Name: {geo.Name}</h4>
-                <h4>Local: {geo.Local}</h4>
-                <h4>Zone Type: {geo.Zone_type}</h4>
-                <h4>Year: {geo.Year}</h4>
-                <h4>Latitude: {geo.lat}</h4>
-                <h4>Longitude: {geo.long}</h4>
+              <div>
+                <p><strong>ID:</strong> {geo.ID}</p>
+                <p><strong>Name:</strong> {geo.Name}</p>
+                <p><strong>Local:</strong> {geo.Local}</p>
+                <p><strong>Zone Type:</strong> {geo.Zone_type}</p>
+                <p><strong>Year:</strong> {geo.Year}</p>
+                <p><strong>Latitude:</strong> {geo.lat}</p>
+                <p><strong>Longitude:</strong> {geo.long}</p>
               </div>
             </Popup>
           </Marker>
         ))}
       </MapContainer>
-
     </div>
   );
 }
