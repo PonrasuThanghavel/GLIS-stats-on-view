b4cf67be470e54c145c5add33b4828946b663e06
Ponrasu Thanghavel <145901854+PonrasuThanghavel@users.noreply.github.com>
2024-04-22 16:37:03 +0530

    updated models

--- backend/Routes/usercreateroute.js
+++ backend/Routes/usercreateroute.js
@@ -33,7 +33,7 @@ router.get('/', async (req, res) => {
     const user = await User.find({});
     res.json(user);
   } catch (error) {
-    console.error('Error fetching user role:', error);
+    console.error('Error fetching users:', error);
     res.status(500).json({ error: 'Internal Server Error' });
   }
 });
