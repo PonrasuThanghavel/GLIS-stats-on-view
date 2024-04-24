4464d448ee81b93ae0d2b8640c272c659af90904
Ponrasu Thanghavel <145901854+PonrasuThanghavel@users.noreply.github.com>
2024-04-24 17:49:53 +0530

    updated model

--- backend/Model/market.js
+++ backend/Model/market.js
@@ -1,22 +1,14 @@
 const mongoose = require('mongoose');
 
 const marketSchema = new mongoose.Schema({
-  ID: { type: Number, default: 0 }, // Set a default value for ID
+  ID: { type: Number },
   Crops: [
     {
       Name: { type: String, required: true },
-      Price: { type: Number, required: true },
+      Price: {type: Number, required: true},
       Quantity: { type: Number, required: true }
     }
   ]
-});
-
-// Pre-save middleware to set a default value for ID if not provided
-marketSchema.pre('save', function(next) {
-  if (!this.ID) {
-    this.ID = 0; // Set a default value for ID if not provided
-  }
-  next();
 });
 
 const MarketModel = mongoose.model('Market', marketSchema);
