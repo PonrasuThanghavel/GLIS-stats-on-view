3b1dcc588f0f2e204842b8c66b7049dc528ca10a
Ponrasu Thanghavel <145901854+PonrasuThanghavel@users.noreply.github.com>
2024-04-24 19:12:05 +0530

    Updated Models and routes

--- backend/Routes/marketRoutes.js
+++ backend/Routes/marketRoutes.js
@@ -1,47 +1,43 @@
-// Backend routes
-
 const express = require('express');
 const router = express.Router();
-const MarketModel = require('../Model/market');
-
-router.use(express.json()); // Middleware to parse JSON requests
+const Market = require('../Model/market');
 
 router.get('/', async (req, res) => {
     try {
-      const marketsData = await MarketModel.find({});
+      const marketsData = await Market.find({});
       res.json(marketsData);
     } catch (error) {
       console.error('Error fetching crops data:', error);
-      res.status(500).json({ error: 'Internal Server Error' });
+      return res.status(500).json({ error: 'Internal Server Error' });
     }
 });
 
 router.get('/:id', async (req, res) => {
   const { id } = req.params;
   try {
-    const marketData = await MarketModel.findOne({ ID: id });
+    const marketData = await Market.findOne({ ID: id });
     if (!marketData) {
-      return res.status(404).json({ message: 'Market data not found for this ID.' });
+      return res.status(404).json({ error: 'Market data not found for this ID.' });
     }
     return res.json(marketData);
   } 
   catch (error) {
     console.error('Error fetching market data:', error);
-    res.status(500).json({ message: 'Internal server error' });
+    return res.status(500).json({ error: 'Internal server error' });
   }
 });
 
 router.post('/:id/crops', async (req, res) => {
   const { id } = req.params;
-  const { Name, Quantity, Price } = req.body; // Ensure consistency in property names
+  const { Name, Quantity, Price } = req.body; 
   try {
-    let marketData = await MarketModel.findOne({ ID: id });
-    if (!marketData) {
+    const marketsData = await Market.findOne({ ID: id });
+    if (!marketsData) {
       return res.status(404).json({ message: 'Market data not found for this ID.' });
     }
-    marketData.Crops.push({ Name, Quantity, Price }); // Update property names
-    await marketData.save();
-    res.status(201).json(marketData);
+    marketsData.Crops.push({ Name, Quantity, Price }); 
+    await marketsData.save();
+    res.status(201).json(marketsData);
   } catch (error) {
     console.error('Error adding crop:', error);
     res.status(500).json({ message: 'Error adding crop to the database.' });
@@ -51,16 +47,16 @@ router.post('/:id/crops', async (req, res) => {
 router.delete('/:id/crops/:cropId', async (req, res) => {
   const { id, cropId } = req.params;
   try {
-    const marketData = await MarketModel.findOne({ ID: id });
-    if (!marketData) {
+    const marketsData = await Market.findOne({ ID: id });
+    if (!marketsData) {
       return res.status(404).json({ message: 'Market data not found for this ID.' });
     }
-    const cropIndex = marketData.Crops.findIndex(crop => crop._id == cropId); // Update property name
+    const cropIndex = marketsData.Crops.findIndex(crop => crop._id == cropId); // Update property name
     if (cropIndex === -1) {
       return res.status(404).json({ message: 'Crop not found in market data.' });
     }
-    marketData.Crops.splice(cropIndex, 1);
-    await marketData.save();
+    marketsData.Crops.splice(cropIndex, 1);
+    await marketsData.save();
     res.json({ message: 'Crop deleted successfully.' });
   } catch (error) {
     console.error('Error deleting crop:', error);
